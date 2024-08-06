e70b18c6179451361aefef2b17d9df2db5b8756f
Hiral Fataniya <150674926+Justheer@users.noreply.github.com>
2024-08-06 12:13:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as a newer varsion
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    + 
+     3) // code readability should be high
+
+
+console.log("Heer");
+
+let name = "heer"
+let age = 18
+let isLoggedIn = false
+
+//number
+//bigint
+//string   => ""
+//boolean  => true or false
+//null  => standalone value
+//undefined
+//symbol  => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
+
+
