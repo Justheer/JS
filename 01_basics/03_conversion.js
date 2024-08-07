2800f229634c683d8fbf82c892b1b625788e080b
Hiral Fataniya <150674926+Justheer@users.noreply.github.com>
2024-08-07 04:39:43 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,33 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//When we convert the number, it will easily convert in number Ex. "33" => 33
+//When we convert the number with word, it will give the value => NaN
+//When we convert the boolean (true), it will convert into the 1 and (false) convert into the 0 Ex. true => 1, false => 0
+
+let isLoggedIn = "heer"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//1 => true
+//0 => false
+//"heer" => true
+//"" => false
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
