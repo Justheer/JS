3534c031cc88a888ba789415dd85434c378fdaec
Hiral Fataniya <150674926+Justheer@users.noreply.github.com>
2024-08-12 09:06:45 +0000

    practice

---
+++ practice1.js
@@ -0,0 +1,61 @@
+//Q.1 List all primitive data types in JavaScript.
+// Ans. String, Number, Boolean, Null, Undefined, BigInt, Symbol.
+
+
+//Q.2 List all reference data types in JavaScript.
+// Ans. Object, Array, Function.
+
+
+//Q.3 Describe the difference between null and undefined.
+// Ans. Undefined : When variable print  without a value,then output will be undefined.
+// Ans. Null : When Variable print with zero value then output will be Null.
+
+
+//Q.4 Show examples of immutable primitive data types.
+// Ans.
+let text = "Heer";
+let age = 18 ; // Number
+let isLoggedIn = false; //Boolean
+let rent = [0]; //Null
+let mark; // undefined
+
+
+//Q.5 Demonstrate how to check the type of a variable using typeof.
+// Ans.
+console.log(typeof text);
+
+
+//Q.6 Explain how JavaScript handles type coercion.
+
+
+//Q.7 Give an example of NaN and explain its use.
+// Ans. 
+let score = 0/0
+console.log(score);
+
+
+
+//Q.8 Compare == and === with examples.
+// Ans.
+let x = 2;
+console.log(x == "2");
+console.log(x === "2");
+
+
+
+
+//Q.9 Practice using the typeof operator with different data types.
+// Ans.
+console.log(typeof text);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof rent);
+console.log(typeof mark);
+
+
+//Q.10 Convert data types using JavaScript functions.
+
+
+
+
+
