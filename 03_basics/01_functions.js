7c64a5ed4ca0439baade398c3cdee8bebf2402e3
Hiral Fataniya <150674926+Justheer@users.noreply.github.com>
2024-08-14 05:49:33 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -14,11 +14,59 @@ function sayMyName() {
 
 function addTwoNumber (number1, number2){
 
-    let result = number1 + number2
-    return result
+   // let result = number1 + number2
+    // return result
+    return number1 + number2    
 }
 
 
 const result = addTwoNumber(3, 5)
 
-console.log("Result:", result );
+//console.log("Result:", result );
+
+function loginUserMessage(username = "sam"){  //sam is a defult value
+
+    if (!username) {
+        console.log("Please enter your username");
+        return
+    }
+
+    return `${username} just logged in`
+}
+
+ //console.log(loginUserMessage("Heer"));
+ //console.log(loginUserMessage("Heer"));
+
+ function calculateCartPrice (val1, val2, ...num1){
+        return num1
+ }
+
+   //console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "heer",
+    price: 199
+}
+
+function handleObject (anyObject){
+   console.log(`user name is ${anyObject.username} and price is ${anyObject.price}`);
+}
+  //handleObject (user)
+   handleObject ({
+        username: "sam",
+        price: 999
+   })  
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue (getArray){
+        return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray)); 
+ console.log(returnSecondValue([200,400, 500, 1000]));
+   
+
+ 
+ 
+   
